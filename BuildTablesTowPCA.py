4191e0c96442cbb7fa898aa4e6fb94844e9929c8
Paul Charpentier <86246436+Paul-Charpentier@users.noreply.github.com>
2023-02-17 17:14:34 +0100

    redo normalization but weigthed

--- BuildTablesTowPCA.py
+++ BuildTablesTowPCA.py
@@ -119,54 +119,111 @@ d2vbinn = np.array(d2vbinn)
 sd2vbinn = np.array(sd2vbinn)
 
 ## Normalization
-#
-# def Mornalize(X, dX):
-#     X_norm = []
-#     X_std = []
-#     for i in tqdm(range(X.shape[0])):
-#         avg_dv = np.nanmean([X[i][j] for j in range(X.shape[1])])
-#         std_dv = np.nanstd([X[i][j] for j in range(X.shape[1])])
-#         Y = []
-#         stdY = []
-#         for j in range(X.shape[1]):
-#             morn = (X[i][j] - avg_dv)/std_dv
-#             std = dX[i][j]/std_dv
-#             Y.append(morn)
-#             stdY.append(std)
-#         X_norm.append(Y)
-#         X_std.append(stdY)
-#     return(np.array(X_norm), np.array(X_std))
-#
-# RV2, dRV2 = Mornalize(d2vbinn, sd2vbinn)
+
+#mask NaNs for the averaging
+ma_dv = np.ma.MaskedArray(d2vbinn, mask=np.isnan(d2vbinn))
+ma_sdv = np.ma.MaskedArray(sd2vbinn, mask=np.isnan(sd2vbinn))
+#Compute average
+avg_dv = np.ma.average(ma_dv, weights=1/ma_sdv**2, axis=1)
+std_dv = np.ma.average((ma_dv-avg_dv.reshape(-1,1))**2, weights=1/ma_sdv**2, axis=1)
+#Reshape average
+avg_dv = avg_dv.data.reshape(-1,1)
+std_dv = np.sqrt(std_dv.data.reshape(-1,1))
+#Normalize
+RV2 = (np.copy(d2vbinn) - avg_dv)/std_dv
+dRV2 = (np.copy(sd2vbinn))/std_dv
+
 
 ## Saves
 
-np.save('/home/paul/Bureau/IRAP/TablesAU_MIC/readyforwPCA_d2vsd2v.npy', [d2vbinn, sd2vbinn])
+np.save('/home/paul/Bureau/IRAP/TablesAU_MIC/readyforwPCA_d2vsd2v.npy', [RV2, dRV2])
 np.save('/home/paul/Bureau/IRAP/TablesAU_MIC/readyforwPCA_linelist.npy', w_used)
 np.save('/home/paul/Bureau/IRAP/TablesAU_MIC/readyforwPCA_epoc.npy', tbinn)
 
-# ## reload
-#
-# RV2, dRV2 = np.load('/home/paul/Bureau/IRAP/TablesAU_MIC/readyforwPCA_d2vsd2v.npy')
-# w_used = np.load('/home/paul/Bureau/IRAP/TablesAU_MIC/readyforwPCA_linelist.npy')
-# times = np.load('/home/paul/Bureau/IRAP/TablesAU_MIC/readyforwPCA_epoc.npy')
-#
-# ## Option 2: Reconstruct w/ wPCA
-#
-# def runPCA(X, dX, ncomp = 2):
-#     X = X.T
-#     dX = dX.T
-#
-#     weight = 1./dX
-#     weight[np.isnan(X)] = 0
-#
-#     pca = WPCA(n_components=ncomp).fit(X, weights = weight)
-#     # Reconstruct the data using the PCA model
-#     Y = WPCA(n_components=ncomp).fit_reconstruct(X, weights = weight)
-#     return(Y)
-#
-# RV2 = runPCA(RV2, dRV2).T
-#
-# ## Save
-#
-# np.save('/home/paul/Bureau/IRAP/TablesAU_MIC/wPCAed_d2v.npy', RV2)
+
+
+## Build Table without any NaNs
+
+#Load Data
+path = '/media/paul/One Touch2/SPIRou_Data/AU_MIC/AUMIC_AUMIC'
+os.chdir(path)
+
+file_list = []
+ALL_d2v = []
+ALL_sd2v = []
+times = []
+#dirs=directories
+# #print('loading ...')
+for (root, dirs, file) in os.walk(path):
+    for f in tqdm(file):
+        if 'lbl.fits' in f:
+            file_list.append(f)
+            nthfile = fits.open(f)
+            times.append(nthfile[0].header['BJD'])
+            ALL_d2v.append(nthfile[1].data['d2v'])
+            ALL_sd2v.append(nthfile[1].data['sd2v'])
+            nthfile.close()
+
+ALL_d2v = np.array(ALL_d2v)
+ALL_sd2v = np.array(ALL_sd2v)
+times = np.array(times)
+
+p1 = fits.open('/media/paul/One Touch2/SPIRou_Data/AU_MIC/AUMIC_AUMIC/2812126o_pp_e2dsff_tcorr_AB_AUMIC_AUMIC_lbl.fits')
+
+w1 = (p1[1].data['WAVE_START']+p1[1].data['WAVE_END'])/2.
+
+# remove all nans
+used_waves = remove_nan(ALL_d2v, threshold=0)
+ALL_d2v = ALL_d2v[:, used_waves]
+ALL_sd2v = ALL_sd2v[:, used_waves]
+w_used = w1[used_waves]
+
+# Night binn
+intnuit = [np.round(t) for t in times]
+i = 1
+while i < len(intnuit):
+    if intnuit[i] in intnuit[:i]:
+        intnuit.pop(i)
+    else :
+        i += 1
+
+tbinn = []
+d2vbinn = []
+sd2vbinn = []
+
+for rjd in tqdm(intnuit):
+    samenight = (np.abs(rjd-times) < 0.5)
+    tbinn.append(np.mean(times[samenight]))
+    Y = []
+    sY = []
+    for l in range(ALL_d2v.shape[1]):
+        y, sy = odd_ratio_mean(ALL_d2v[samenight, l], ALL_sd2v[samenight, l])
+
+        Y.append(y)
+        sY.append(sy)
+    d2vbinn.append(Y)
+    sd2vbinn.append(sY)
+
+d2vbinn = np.array(d2vbinn)
+sd2vbinn = np.array(sd2vbinn)
+
+# normalize
+#mask NaNs for the averaging
+ma_dv = np.ma.MaskedArray(d2vbinn, mask=np.isnan(d2vbinn))
+ma_sdv = np.ma.MaskedArray(sd2vbinn, mask=np.isnan(sd2vbinn))
+#Compute average
+avg_dv = np.ma.average(ma_dv, weights=1/ma_sdv**2, axis=1)
+std_dv = np.ma.average((ma_dv-avg_dv.reshape(-1,1))**2, weights=1/ma_sdv**2, axis=1)
+#Reshape average
+avg_dv = avg_dv.data.reshape(-1,1)
+std_dv = std_dv.data.reshape(-1,1)
+#Normalize
+RV2 = (np.copy(d2vbinn) - avg_dv)/std_dv
+dRV2 = (np.copy(sd2vbinn) - avg_dv)/std_dv
+
+
+# save
+
+np.save('/home/paul/Bureau/IRAP/TablesAU_MIC/readyforwPCA_d2vsd2v_NoNaN.npy', [RV2, dRV2])
+np.save('/home/paul/Bureau/IRAP/TablesAU_MIC/readyforwPCA_linelist_NoNaN.npy', w_used)
+np.save('/home/paul/Bureau/IRAP/TablesAU_MIC/readyforwPCA_epoc_NoNaN.npy', tbinn)
